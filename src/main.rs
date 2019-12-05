8aed59aac6c7001cc6ca9232551dc1e5e10acd1c
Piyush Rungta <piyushrungta25@gmail.com>
2019-12-06 01:37:29 +0530

    Fix bugs
    Lots of bugs

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,8 @@ enum Instruction {
     SetRegisterAND(usize, usize),
     SetRegisterXOR(usize, usize),
     AddRegisterToRegister(usize, usize),
-    SubRegisterToRegister(usize, usize),
+    SubRegisterToRegister85(usize, usize),
+    SubRegisterToRegister87(usize, usize),
     ShiftRight(usize),
 
     ShiftLeft(usize),
@@ -120,16 +121,16 @@ impl Chip8 {
     }
 
     fn decode(&mut self, oc: Opcode) -> Instruction {
-        let reg1: usize = ((oc & 0x0F00)/(16*16)) as usize;
-        let reg2: usize = ((oc & 0x00F0)/16) as usize;
+        let reg1: usize = ((oc & 0x0F00) >> 8) as usize;
+        let reg2: usize = ((oc & 0x00F0) >> 4) as usize;
         let nnn: usize = (oc & 0x0FFF) as usize;
         let nn: u8 = (oc & 0x00FF) as u8;
 
 
         return match oc & 0xF000 {
-            0x0000 => match oc & 0x000F {
-                0x0000 => Instruction::ClearScreen,
-                0x000E => Instruction::Return,
+            0x0000 => match oc & 0x00FF {
+                0x00E0 => Instruction::ClearScreen,
+                0x00EE => Instruction::Return,
                 _ => Instruction::Noop,
             },
             0x1000 => Instruction::JumpTo(nnn),
@@ -150,10 +151,10 @@ impl Chip8 {
                 0x0002 => Instruction::SetRegisterAND(reg1, reg2),
                 0x0003 => Instruction::SetRegisterXOR(reg1, reg2),
                 0x0004 => Instruction::AddRegisterToRegister(reg1, reg2),
-                0x0005 => Instruction::SubRegisterToRegister(reg1, reg2),
+                0x0005 => Instruction::SubRegisterToRegister85(reg1, reg2),
                 0x0006 => Instruction::ShiftRight(reg1),
-                0x0007 => Instruction::SubRegisterToRegister(reg2, reg1),
-                0x0008 => Instruction::ShiftLeft(reg1),
+                0x0007 => Instruction::SubRegisterToRegister87(reg1, reg2),
+                0x000E => Instruction::ShiftLeft(reg1),
                 _ => Instruction::Noop,
             },
 
@@ -202,7 +203,7 @@ impl Chip8 {
             }
             Instruction::JumpTo(addr) => self.pc = addr,
             Instruction::Subroutine(addr) => {
-                self.call_stack.push(self.pc);
+                self.call_stack.push(self.pc+2);
                 self.pc = addr;
             }
             Instruction::SkipIfRegisterEqualValue(reg, val) => {
@@ -229,7 +230,7 @@ impl Chip8 {
             }
             Instruction::AddRegisterValue(reg, val) => {
                 self.pc += 2;
-                self.registers[reg] = val;
+                self.registers[reg] = self.registers[reg].wrapping_add(val);
             }
             Instruction::SetRegister(reg1, reg2) => {
                 self.pc += 2;
@@ -251,15 +252,23 @@ impl Chip8 {
                 self.pc += 2;
                 let (res, overflow) = self.registers[reg1].overflowing_add(self.registers[reg2]);
                 self.registers[reg1] = res;
-                self.registers[15] = if overflow { 1 } else { 2 };
+                self.registers[15] = if overflow { 1 } else { 0 };
             }
-            Instruction::SubRegisterToRegister(reg1, reg2) => {
+            Instruction::SubRegisterToRegister85(reg1, reg2) => {
+                self.pc += 2;
+                let vx = self.registers[reg1];
+                let vy = self.registers[reg2];
+
+                self.registers[15] = if vx < vy { 1 } else { 0 };
+                self.registers[reg1] = vx.wrapping_sub(vy);
+            }
+            Instruction::SubRegisterToRegister87(reg1, reg2) => {
                 self.pc += 2;
                 let vx = self.registers[reg1];
                 let vy = self.registers[reg2];
 
                 self.registers[15] = if vx > vy { 1 } else { 0 };
-                self.registers[reg1] = vx.wrapping_sub(vy);
+                self.registers[reg1] = vy.wrapping_sub(vx);
             }
             Instruction::ShiftRight(reg) => {
                 self.pc += 2;
@@ -303,11 +312,11 @@ impl Chip8 {
                         let new_val: bool  = ( (word >> (7-j)) % 2) != 0;
                         let tx = x + j;
                         let ty = y + i;
-                        let cur_val = self.pixel_buffer[ty][tx];
+                        let cur_val = self.pixel_buffer[ty%32][tx%64];
                         if cur_val != new_val {
                             did_overflow = true;
                         }
-                        self.pixel_buffer[ty][tx] = new_val;
+                        self.pixel_buffer[ty%32][tx%64] = new_val;
                     }
                 }
 
@@ -346,7 +355,7 @@ impl Chip8 {
                 self.pc += 2;
                 let (res, overflow) = self.index.overflowing_add(self.registers[reg] as usize);
                 self.index = res;
-                self.registers[15] = if overflow { 1 } else { 2 };
+                self.registers[15] = if overflow { 1 } else { 0 };
             }
             Instruction::SetIndexToSpriteAddr(reg) => {
                 self.pc += 2;
@@ -355,21 +364,20 @@ impl Chip8 {
             }
             Instruction::BCD(reg) => {
                 self.pc += 2;
-                let mut vx = self.registers[reg];
-                for i in 0..3 {
-                    self.memory[self.index + 2 - i] = vx % 10;
-                    vx /= 10;
-                }
+                let vx = self.registers[reg];
+                self.memory[self.index] = vx/100;
+                self.memory[self.index + 1] = (vx/10)%10;
+                self.memory[self.index + 2] = vx%10;
             }
             Instruction::DumpRegistersTill(reg) => {
                 self.pc += 2;
-                for i in 0..(reg as u8) {
+                for i in 0..=(reg as u8) {
                     self.memory[self.index + (i as usize)] = self.registers[i as usize];
                 }
             }
             Instruction::LoadRegistersTill(reg) => {
                 self.pc += 2;
-                for i in 0..(reg as u8) {
+                for i in 0..=(reg as u8) {
                     self.registers[i as usize] = self.memory[self.index + (i as usize)];
                 }
             }
@@ -423,7 +431,7 @@ impl Chip8 {
     }
 
     fn sleep() {
-        thread::sleep(time::Duration::from_millis(16));
+        thread::sleep(time::Duration::from_millis(1));
     }
 
     fn load_rom(&mut self, data: Vec<u8>) {
@@ -450,12 +458,13 @@ fn main() {
     canvas.present();
     let mut c8 = Chip8::new(canvas);
 
-    // let mut data: Vec<u8> = Vec::new();
+    let mut data: Vec<u8> = Vec::new();
     // File::open("roms/INVADERS").unwrap().read_to_end(&mut data);
     // File::open("roms/UFO").unwrap().read_to_end(&mut data);
+    File::open("roms/TETRIS").unwrap().read_to_end(&mut data);
 
     // this should put 5 in top left corner
-    let mut data: Vec<u8> = vec![0x00, 0xE0, 0x61, 0x0d, 0xF1, 0x29, 0x61, 0x00, 0x62, 0x00, 0xD1, 0x25, 0x00, 0x0F, 0x12, 0x0C];
+    // let mut data: Vec<u8> = vec![0x00, 0xE0, 0x61, 0x0d, 0xF1, 0x29, 0x61, 0x3e, 0x62, 0x03, 0xD1, 0x25, 0x00, 0x0F, 0x12, 0x0C];
 
     c8.load_rom(data);
     let mut event_pump = sdl_context.event_pump().unwrap();
